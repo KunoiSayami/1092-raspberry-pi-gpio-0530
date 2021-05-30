7b0a82cdae13edcfcc3abf7eab57488369e65677
KunoiSayami <46131041+KunoiSayami@users.noreply.github.com>
2021-05-30 21:56:04 +0800

    init

---
+++ api_server.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# api_server.py
+# Copyright (C) 2021 KunoiSayami
+#
+# This module is part of 1092-raspberry-pi-gpio-0530 and is released under
+# the AGPL v3 License: https://www.gnu.org/licenses/agpl-3.0.txt
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+from __future__ import annotations
+import asyncio
+import base64
+import io
+import logging
+from threading import Condition
+import weakref
+import sys
+import concurrent.futures
+import signal
+import os
+from types import FrameType
+from typing import Optional
+
+from aiohttp import web
+import aiohttp
+
+from gpio import LEDGPIO
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
+class Server:
+    def __init__(self, host: str, port: int):
+        self.host = host
+        self.port = port
+        self.gpio: Optional[LEDGPIO] = None
+        self.website = web.Application()
+        self.bind = host
+        self.port = port
+        self.site = None
+        self.runner = web.AppRunner(self.website)
+        self._idled = False
+
+    async def light_control(self, _request: web.Request) -> web.Response:
+        await self.gpio.set_light_flash(3, custom_pins=self.gpio.pins)
+        return web.json_response(dict(status=200))
+
+    async def breath_control(self, _request: web.Request) -> web.Response:
+        await self.gpio.set_light_breath()
+        return web.json_response(dict(status=200))
+
+    async def show_number(self, request: web.Request) -> web.Response:
+        req = await request.json()
+        if (num := req.get('number')) is not None:
+            try:
+                await self.gpio.show_number(int(num))
+            except ValueError:
+                logger.exception('Got exception:')
+                return web.json_response(dict(reason='Check your value'), status=400)
+        return web.json_response(dict(status=200))
+
+    @staticmethod
+    async def hello(_request: web.Request) -> web.Response:
+        return web.json_response(dict(status=200))
+
+    async def start(self) -> None:
+        self.gpio = LEDGPIO()
+        self.website.router.add_get('/', self.hello)
+        self.website.router.add_post('/light', self.light_control)
+        self.website.router.add_post('/number', self.show_number)
+        self.website.router.add_post('/breath', self.breath_control)
+        self.website.on_shutdown.append(self.handle_web_shutdown)
+        await self.runner.setup()
+        self.site = web.TCPSite(self.runner, self.bind, self.port)
+        await self.site.start()
+
+    @staticmethod
+    async def handle_web_shutdown(_app: web.Application) -> None:
+        pass
+
+    async def idle(self):
+        self._idled = True
+
+        for sig in (signal.SIGINT, signal.SIGABRT, signal.SIGTERM):
+            signal.signal(sig, self._reset_idle)
+
+        while self._idled:
+            await asyncio.sleep(1)
+
+    def _reset_idle(self, signal_: signal.Signals, _frame_type: FrameType) -> None:
+        if not self._idled:
+            logger.debug('Got signal %s, killing...', signal_)
+            os.kill(os.getpid(), signal.SIGKILL)
+        else:
+            logger.debug('Got signal %s, stopping...', signal_)
+            self._idled = False
+
+    async def stop(self) -> None:
+        await self.site.stop()
+        await self.runner.cleanup()
+        await self.gpio.close()
+
+
+def get_argument(arg: str, default: Optional[str]) -> Optional[str]:
+    try:
+        index = sys.argv.index(arg)
+        return sys.argv[index + 1]
+    except (ValueError, IndexError):
+        pass
+    return default
+
+
+async def main():
+    server = Server(get_argument('--host', '0.0.0.0'), int(get_argument('--port', '8081')))
+    await server.start()
+    await server.idle()
+    await server.stop()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(levelname)s - %(name)s - %(funcName)s - %(lineno)d - %(message)s')
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.run_until_complete(asyncio.sleep(0.25))
+    loop.close()
---
+++ gpio.py
@@ -0,0 +1,203 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# gpio.py
+# Copyright (C) 2021 KunoiSayami
+#
+# This module is part of 1092-raspberry-pi-gpio-0530 and is released under
+# the AGPL v3 License: https://www.gnu.org/licenses/agpl-3.0.txt
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+import asyncio
+import concurrent.futures
+import logging
+from typing import Optional, Sequence
+
+import RPi.GPIO as GPIO
+import Adafruit_DHT
+
+
+logger = logging.getLogger('gpio.gpio')
+logger.setLevel(logging.DEBUG)
+
+
+class StandalonePWM:
+
+    def __init__(self, pin: int):
+        self.stop_request = asyncio.Event()
+        self.pwm = GPIO.PWM(pin, 50)
+        self.pwm.start(0)
+        self._future: Optional[concurrent.futures.Future] = None
+
+    async def runnable(self) -> None:
+        while not self.stop_request.is_set():
+            for dc in range(10):
+                self.pwm.ChangeDutyCycle(dc * 10)
+                await asyncio.sleep(.2)
+            await asyncio.sleep(1)
+            for dc in range(10, 0, -1):
+                self.pwm.ChangeDutyCycle(dc * 10)
+                await asyncio.sleep(.2)
+            await asyncio.sleep(1)
+        self.pwm.stop()
+        self.pwm = None
+
+    def set_stop(self) -> None:
+        self.stop_request.set()
+
+    def run(self) -> concurrent.futures.Future:
+        self._future = asyncio.run_coroutine_threadsafe(self.runnable(), asyncio.get_event_loop())
+        return self._future
+
+    @property
+    def future(self) -> concurrent.futures.Future:
+        return self._future
+
+    def force_stop(self):
+        self._future.cancel()
+        if self.pwm:
+            self.pwm.stop()
+            self.pwm = None
+
+
+class LEDGPIO:
+    pins = [17, 27, 22, 5, 6, 13, 19, 26]
+    lock = asyncio.Lock()
+    event = asyncio.Event()
+    stop_event = asyncio.Event()
+
+    def __init__(self) -> None:
+        self.clean_required = asyncio.Event()
+        self.breath_pwm: Optional[StandalonePWM] = None
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.pins, GPIO.OUT)
+
+    @staticmethod
+    async def _unsafe_set_light_flash(pins: Sequence[int]) -> None:
+        for pin in pins:
+            GPIO.output(pin, GPIO.HIGH)
+        await asyncio.sleep(0.2)
+        for pin in pins:
+            GPIO.output(pin, GPIO.LOW)
+
+    async def set_light_flash(
+            self,
+            times: int = 1,
+            *,
+            custom_pins: Sequence[int] = None,
+            is_odd: bool = True) -> None:
+        if custom_pins is None:
+            pins = self.pins[int(not is_odd):: 2]
+        else:
+            pins = custom_pins
+        await self._set_light_flash_num(times, pins)
+
+    async def _set_light_flash_num(self, times: int, pins: Sequence[int]) -> None:
+        if self.clean_required.is_set():
+            await self.clean_number()
+        async with self.lock:
+            self.event.set()
+            self.stop_event.clear()
+            for _ in range(times):
+                if self.stop_event.is_set():
+                    break
+                await self._unsafe_set_light_flash(pins)
+            self.event.clear()
+
+    @staticmethod
+    async def _unsafe_light_breath(pwms: Sequence[GPIO.PWM]) -> None:
+        for dc in range(10):
+            for pwm in pwms:
+                pwm.ChangeDutyCycle(dc * 10)
+            await asyncio.sleep(.05)
+        await asyncio.sleep(.05)
+        for dc in range(10, 0, -1):
+            for pwm in pwms:
+                pwm.ChangeDutyCycle(dc * 10)
+            await asyncio.sleep(.05)
+
+    async def set_light_breath(
+            self,
+            times: Optional[int] = 1,
+            pins: Optional[Sequence[int]] = None
+    ) -> None:
+        if pins is None:
+            pins = self.pins
+        else:
+            pins = [self.pins[x - 1] for x in pins]
+        if self.clean_required.is_set():
+            await self.clean_number()
+        async with self.lock:
+            self.event.set()
+            self.stop_event.clear()
+            pwms = [GPIO.PWM(pin, 50) for pin in pins]
+            for pwm in pwms:
+                pwm.start()
+            if times is None:
+                while True:
+                    if self.stop_event.is_set():
+                        break
+                    await self._unsafe_light_breath(pwms)
+                    await asyncio.sleep(.05)
+            else:
+                for _ in range(times):
+                    if self.stop_event.is_set():
+                        break
+                    await self._unsafe_light_breath(pwms)
+                    await asyncio.sleep(.05)
+            for pwm in pwms:
+                pwm.stop()
+            self.event.clear()
+
+    async def show_number(self, number: int, show_breath: bool = True) -> None:
+        print(number)
+        if not (128 > number > 0):
+            raise ValueError('Number should smaller then 128 or bigger then 0')
+        async with self.lock:
+            self.event.set()
+            if show_breath:
+                self.breath_pwm = StandalonePWM(self.pins[0])
+                self.breath_pwm.run()
+            self.clean_required.set()
+            b = f'{int(bin(number)[2:]):07}'
+            for i in range(7):
+                GPIO.output(self.pins[i + 1], GPIO.HIGH if int(b[i]) else GPIO.LOW)
+            self.event.clear()
+
+    async def clean_number(self) -> None:
+        async with self.lock:
+            if self.breath_pwm:
+                self.breath_pwm.set_stop()
+                try:
+                    self.breath_pwm.future.result(1)
+                except concurrent.futures.TimeoutError:
+                    self.breath_pwm.force_stop()
+            for pin in self.pins:
+                GPIO.output(pin, GPIO.LOW)
+            self.clean_required.clear()
+
+    async def close(self) -> None:
+        if self.event.is_set():
+            raise RuntimeError('Some work is still running')
+        if self.clean_required:
+            await self.clean_number()
+        GPIO.cleanup()
+
+
+class DHTSensor:
+    DHT_SENSOR = Adafruit_DHT.DHT22
+    DHT_PIN = 12
+
+    def get_data(self) -> tuple[float, float]:
+        humidity, temperature = Adafruit_DHT.read_retry(self.DHT_SENSOR, self.DHT_PIN)
+        return humidity, temperature
